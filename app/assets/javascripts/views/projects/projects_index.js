ba72077226d619aacc2b0f7901ea2bde252bcb6c
Duncan Skinner <skinnerduncan@gmail.com>
2014-11-26 08:10:33 -0800

    make projects index view composite

--- app/assets/javascripts/views/projects/projects_index.js
+++ app/assets/javascripts/views/projects/projects_index.js
@@ -2,12 +2,24 @@ FinalProject.Views.ProjectsIndex = Backbone.View.extend({
 	template: JST['projects/index'],
 	
 	initialize: function () {
-		this.listenTo(this.collection, 'sync', this.render);
+		this.listenTo(this.collection, 
+			'add change:title change:description remove reset', this.render);
+		// will everything be re-rendered upon sync event?
 	},
 	
 	render: function () {
-		var content = this.template({projects: this.collection});
+		// var content = this.template({projects: this.collection});
+		// this.$el.html(content);
+		// return this;
+		var content = this.template();
 		this.$el.html(content);
+		var that = this;
+		this.collection.each(function (project) {
+			var itemView = new FinalProject.Views.ProjectsIndexItem({
+				model: project
+			});
+			that.$el.append(itemView.render().$el);
+		});
 		return this;
 	},
 });
---
+++ app/assets/javascripts/views/projects/projects_index_item.js
@@ -0,0 +1,13 @@
+FinalProject.Views.ProjectsIndexItem = Backbone.View.extend({
+	template: JST['projects/item'],
+	
+	initialize: function () {
+		this.listenTo(this.model, 'sync', this.render);
+	},
+
+	render: function () {
+		var content = this.template({project: this.model});
+		this.$el.html(content);
+		return this;
+	},
+});
