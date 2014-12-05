31d008d2846de1d3e372c7600bf4e99accfa8f27
Duncan Skinner <skinnerduncan@gmail.com>
2014-12-05 10:05:22 -0800

    add a footer

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -20,7 +20,8 @@ FinalProject.Routers.Router = Backbone.Router.extend({
 			this._currentView.remove();
 		}
 		this._currentView = homeView;
-		$('body').append(homeView.render().$el);
+		// $('body').append(homeView.render().$el);
+		(homeView.render().$el).insertBefore($('#footer'));
 	},
 	
 	projectsIndex: function () {
